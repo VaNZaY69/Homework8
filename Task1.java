cf62bc6e92c80cb0f854b2a657146a3b5aad7e29
Ivan Zaets <ivan.i.zaets@gmail.com>
2021-04-23 17:46:54 +0300

    Fixed notes to the code for the first task

--- Task1.java
+++ Task1.java
@@ -1,4 +1,5 @@
 package com.Homework8;
+
 /*
 1) Электронные часы показывают время в формате от 00:00 до 23:59.
 Подсчитать сколько раз за сутки случается так, что слева от двоеточия
@@ -8,24 +9,13 @@ package com.Homework8;
 public class Task1 {
     public static void main(String[] args) {
         int result = 0;
-        String strhours;
-        String strminutes;
 
-        for (int hours = 0; hours <= 23; hours++) {
-            for (int minutes = 0; minutes <= 59; minutes++) {
-                if (hours < 10) {
-                    strhours = "0" + hours;
-                } else {
-                    strhours = String.valueOf(hours);
-                }
-                if (minutes < 10) {
-                    strminutes = "0" + minutes;
-                } else {
-                    strminutes = String.valueOf(minutes);
-                }
-                StringBuilder tmp = new StringBuilder(strhours);
-                if (tmp.reverse().toString().equals(strminutes)) {
-                    System.out.println(strhours + ":" + strminutes);
+        for (int i = 0; i < 24; i++) {
+            for (int j = 0; j < 60; j++) {
+                String hoursDisplay = String.format("%02d", i);
+                String minutesDisplay = String.format("%02d", j);
+                String reversedHours = new StringBuilder(hoursDisplay).reverse().toString();
+                if (minutesDisplay.equals(reversedHours)) {
                     result++;
                 }
             }
